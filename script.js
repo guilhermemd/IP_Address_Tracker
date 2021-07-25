bc4b857aaec9c13a2454af4ad613abbf6df60898
guilhermemd <guilherme.md@gmail.com>
2021-07-25 19:10:35 -0300

    refactor: :sparkles: half way to finish the map with lat and lng

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const btnIpSearch = document.getElementById("btn_ip_search");
+function renderMap (lat, lng) {
+  let mymap = L.map('mapid').setView([lat, lng], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZ3VpbGhlcm1lbWQiLCJhIjoiY2tyanE1OTRyMHN0NjJxa2RhdTVqczB5NiJ9.p_jM5xrwXD7DP1nfI0PuRw'
+}).addTo(mymap);
+}
+renderMap(51.505, -0.09);
 
 async function fetchApi(param) {
+  const mapDiv = document.getElementById("mapid");
+  mapDiv.remove();
+  const mapDiv2 = document.createElement("div").setAttribute("id", "mapid");
+  document.body.appendChild(mapDiv2);
+
   let url = `
   https://geo.ipify.org/api/v1?apiKey=at_LDZhbJcnGgV7mnIZ9vV0E82Ubm429&ipAddress=${param}`;
 
@@ -14,6 +31,7 @@ async function fetchApi(param) {
   utc.textContent = `UTC ${data.location.timezone}`;
   const isp = document.getElementById("isp");
   isp.textContent = data.isp;
+  renderMap(data.location.lat, data.location.lng);
 }
 
 btnIpSearch.addEventListener("click", () => {
