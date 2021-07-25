405a75c720fee730a4e023d286a28deae7b2c6d9
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-07-25 17:05:09 -0300

    change endpoint ip search

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const btnIpSearch = document.getElementById("btn_ip_search");
 
 async function fetchApi(param) {
-  let url = `https://geo.ipify.org/api/v1?apiKey=at_Wd99BZuqQULBmHMT5cOt9OFhIgPGW&ipAddress=${param}`;
+  let url = `
+  https://geo.ipify.org/api/v1?apiKey=at_LDZhbJcnGgV7mnIZ9vV0E82Ubm429&ipAddress=${param}`;
 
   const response = await fetch(url);
   const data = await response.json();
